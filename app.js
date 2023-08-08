1d5a51e09f366a225ba6a239efdca6f5a3fc79ab
usman zafar <musman.zafar@systemsltd.com>
2023-08-08 14:55:05 +0500

    second push

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
-const http = require('http');
-
-const fs = require('fs');
-
-
-
-const server = http.createServer((req, res) => {
-    console.log(req.url);
-    if (req.url == "/") {
-        return res.end("Hello from '/' route");
-    }
-    else if (req.url == "/about") {
-        return res.end("Hello from '/about' route");
-    }
-    else if (req.url == "/contact") {
-        return res.end("Hello from '/contact' route");
-    }
-    else if (req.url == "/readFile") {
-        fs.readFile('./test.txt', 'utf8', (err, data) => {
-            if (err) {
-              console.error(err);
-              return;
-            }
-            return res.end(data);
-          });
-    }
-    else {
-        res.writeHead(404, {"Content-type": "text/html"});
-        return res.end("<h1>404 Page Not Found</h1>");
-    }
-});
-
-server.listen(3000);
+ const http = require('http');
+ 
+ const fs = require('fs');
+ 
+ 
+ 
+ const server = http.createServer((req, res) => {
+     console.log(req.url);
+     if (req.url == "/") {
+         return res.end("Hello from '/' route");
+     }
+     else if (req.url == "/about") {
+         return res.end("Hello from '/about' route");
+     }
+     else if (req.url == "/contact") {
+         return res.end("Hello from '/contact' route");
+     }
+     else if (req.url == "/readFile") {
+         fs.readFile('./test.txt', 'utf8', (err, data) => {
+             if (err) {
+               console.error(err);
+               return;
+             }
+             return res.end(data);
+           });
+     }
+     else {
+         res.writeHead(404, {"Content-type": "text/html"});
+         return res.end("<h1>404 Page Not Found</h1>");
+     }
+ });
+ 
+ server.listen(3000);
